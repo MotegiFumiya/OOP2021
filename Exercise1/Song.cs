3dd8babec3130aec66a5a10a201b4aed2490b4b2
茂木 郁也 <31003@ojsacjp.onmicrosoft.com>
2021-06-01 09:29:50 +0900

    Exercise2.1

---
+++ Exercise1/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+        //ソングクラス
+    class Song {
+        //歌のタイトル
+        public string Title{ get; set; }
+        //アーティストネーム
+        public string ArtistName { get; set; }
+        //演奏時間　単位:秒
+        public int Length { get; set; }
+
+        public Song(string title,string artistname,int length) {
+            this.Title = title;
+            this.ArtistName = artistname;
+            this.Length = length;
+        }
+    }
+}
