232e5c67406428c967541e5e1345909cccdde038
茂木 郁也 <31003@ojsacjp.onmicrosoft.com>
2021-06-01 11:13:12 +0900

    Exercise2.3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("sales.csv");
+
+            //var amountPerStore = sales.GetPerStoreSaled();
+            var amountPerCategory = sales.GetPerCategorySaled();
+            foreach (var obj in amountPerCategory) {
+                Console.WriteLine("{0}{1:#,0}円", obj.Key, obj.Value);
+            }
+        }
+    }
+}
