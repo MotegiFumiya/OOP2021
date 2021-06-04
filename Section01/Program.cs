2598c6fe52c61e6baa434dd6712541c12537c5b6
茂木 郁也 <31003@ojsacjp.onmicrosoft.com>
2021-06-04 15:51:18 +0900

    Exercise3

--- Section01/Program.cs
+++ Section01/Program.cs
@@ -8,6 +8,21 @@ namespace Section01 {
     class Program {
         static void Main(string[] args) {
             var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+            var names = new List<string> { "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong" };
+            //string[] names = {
+            //    "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra"  };
+            //var query = names.Where(s => s.Length <= 5).ToList();
+
+            //query.ForEach(Console.WriteLine);
+
+            //foreach (var item in query)
+            //    Console.WriteLine(item);
+            //Console.WriteLine("------");
+
+            //names[0] = "Osaka";
+
+            //foreach (var item in query)
+            //    Console.WriteLine(item);
             //3.1.1
             Exercise1_1(numbers);
             Console.WriteLine("-----");
@@ -24,21 +39,61 @@ namespace Section01 {
             Exercise1_4(numbers);
             Console.WriteLine("-----");
 
-            //string[] names = {
-            //    "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra"  };
-            //var query = names.Where(s => s.Length <= 5).ToList();
+            //3.2.1
+            Exercise2_1(names);
+            Console.WriteLine("-----");
 
-            //query.ForEach(Console.WriteLine);
+            //3.2.2
+            Exercise2_2(names);
+            Console.WriteLine("-----");
 
-            //foreach (var item in query)
-            //    Console.WriteLine(item);
-            //Console.WriteLine("------");
+            //3.2.3
+            Exercise2_3(names);
+            Console.WriteLine("-----");
 
-            //names[0] = "Osaka";
+            //3.2.4
+            Exercise2_4(names);
+            Console.WriteLine("-----");
 
-            //foreach (var item in query)
-            //    Console.WriteLine(item);
+            //できた人用
+            Exercise2_5(names);
+
         }
+
+        private static void Exercise2_5(List<string> names) {
+            int count = 0;
+            foreach (var name in names) {
+                count+=name.Count(c => char.IsLower(c));
+            }
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.WriteLine("都市名を入力>");
+            var line = Console.ReadLine();
+            var index= names.FindIndex(s => s == line);
+            Console.WriteLine(index);
+        }
+
+        private static void Exercise2_2(List<string> names) {
+            var count = names.Count(s => s.Contains("o"));
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            IEnumerable<string> query = names.Where(s => s.Contains("o"));
+            foreach (string s in query)
+                Console.WriteLine(s);
+        }
+
+        private static void Exercise2_4(List<string> names) {
+            var query = names.Where(s => s.StartsWith("B")).Select(s=>s.Length);//names.Where(s=>s.StartsWith("B")).Select(s=>s.Length)
+            foreach(var s in query) {
+                Console.WriteLine(s);
+            }
+            
+        }
+
 
         private static void Exercise1_1(List<int> numbers) {
             var exist = numbers.Exists(n => n % 8 == 0 || n % 9 == 0);
@@ -55,11 +110,14 @@ namespace Section01 {
         }
         private static void Exercise1_3(List<int> numbers) {
             IEnumerable<int> query = numbers.Where(n2 => n2 >= 50);
-            foreach (int s in query)
+            foreach (int n2 in query)
                 Console.WriteLine(n2);
         }
-        private static void Exercise1_4(List<int> numbers) {                        
-                       
+        private static void Exercise1_4(List<int> numbers) {
+            List<int> list = numbers.Select(n3=>n3 * 2).ToList();
+            foreach(var num in list) {
+                Console.WriteLine(num);
+            }
         }
 
 
