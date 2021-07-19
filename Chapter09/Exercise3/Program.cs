0a136e0a93f7b9c566ff7c4020f67d84231c647c
郁也 茂木 <31003@ojsacjp.onmicrosoft.com>
2021-07-19 15:56:36 +0900

    夏休み前最終授業

---
+++ Chapter09/Exercise3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var lines = new[] { "====" };
+            var filePath = @"output.txt";
+            using (var writer = new StreamWriter(filePath, append: true)) {
+                foreach (var line in lines)
+                    writer.WriteLine(line);
+                
+            }
+        }
+    }
+}
