57f0a725c09e08cc072411542e2bddbf02e1d093
郁也 茂木 <31003@ojsacjp.onmicrosoft.com>
2021-08-27 15:15:31 +0900

    リスト11.7まで 8.27

---
+++ Section11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section11 {
+    class Program {
+        static void Main(string[] args) {
+
+            var xdoc = XDocument.Load("novellists.xml");
+            
+
+            foreach(var xnovelist in xdoc.Elements()) {
+                var xname =xnovelist.Element("name");
+                var works = xnovelist.Elements("masterpieces")
+                                        .Elements("title")
+                                        .Select(x => x.Value);
+                Console.WriteLine("{0}{1}", xname.Value, string.Join(",",works));
+            }
+        }
+    }
+}
