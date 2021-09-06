7a8cb05892d42340b8be38593d4dbc056402e045
郁也 茂木 <31003@ojsacjp.onmicrosoft.com>
2021-09-06 16:27:39 +0900

    タイトル表示未完成

---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        private object cityCode;
+
+        public Form1() {
+            InitializeComponent();
+            SetRssTitle();
+        }
+
+        private void SetRssTitle() {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+        //指定したURL先からXMLデータを取得しtitle要素を取得し、リストボックスへセットする
+        private void SetRssTitle(string uri) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                    @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml");
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+
+                //foreach (var node in nodes) {
+                //    //string s = Regex.Replace(node.Value, "【|】| - Yahoo!天気・災害", "");
+                //    //yield return s;
+                //}
+            }
+        }
+
+        private void btRead_Click(object sender, EventArgs e) {
+            lbTitles.Items.Add("http://rss.weather.yahoo.co.jp/rss/days/{0}.xml");
+        }
+
+        private void lbTitles_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
+
+        }
+    }
+}
+        
