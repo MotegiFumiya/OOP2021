e94abd861edabcaafc79374abf3be3da9fb71f0f
郁也 茂木 <31003@ojsacjp.onmicrosoft.com>
2021-10-21 10:52:42 +0900

    13章演習問題回答例

---
+++ Chapter14/Section04_json/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Section04_json {
+    class Program {
+        static void Main(string[] args) {
+            var keyword = "算用記";
+            var content = GetFromWikipedea(keyword);
+            Console.WriteLine(content ?? "見つかりませんでした");
+        }
+
+        private static string GetFromWikipedea(string keyword) {
+            var wc = new WebClient();
+            wc.QueryString = new NameValueCollection() {
+                ["action"] = "query",
+                ["prop"] = "revisions",
+                ["rvprop"] = "content",
+                ["format"] = "json",
+                //["format"] = "xml",
+                ["titles"] = HttpUtility.UrlEncode(keyword, Encoding.UTF8),
+            };
+            wc.Headers.Add("Content-type", "charset=UTF-8");
+            var result = wc.DownloadString("http://ja.wikipedia.org/w/api.php");
+            var xmldoc = XDocument.Parse(result);
+            var rev = xmldoc.Root.Descendants("rev").FirstOrDefault();
+            return HttpUtility.HtmlDecode(rev?.Value);
+        }
+    }
+
+
+}
---
+++ RssReader/RssReader/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+<<<<<<< HEAD
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
+=======
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        private object cityCode;
+
+        public Form1() {
+            InitializeComponent();
+            
+        }
+
+        private void SetRssTitle(TextBox tbUrl) {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+        //指定したURL先からXMLデータを取得しtitle要素を取得し、リストボックスへセットする
+        private void SetRssTitle(string uri) {
+            using (var wc = new WebClient()) {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                    @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml");
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+
+                lbTitles.Items.Add(nodes);
+
+                //foreach (var node in nodes) {
+                //    //string s = Regex.Replace(node.Value, "【|】| - Yahoo!天気・災害", "");
+                //    //yield return s;
+                //}
+            }
+        }
+
+        private void btRead_Click(object sender, EventArgs e) {
+            //XDocument xdoc = XDocument.Load(stream);
+            //var nodes = xdoc.Root.Descendants("title");
+
+            SetRssTitle(tbUrl.Text);
+
+            //lbTitles.Items.Add("http://rss.weather.yahoo.co.jp/rss/days/{0}.xml");
+
+            
+        }
+
+        private void lbTitles_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) {
+
+        }
+    }
+}
+        
+>>>>>>> e451921fd6437013de1e3588e109540bee0a278f
